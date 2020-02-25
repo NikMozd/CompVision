9c9650836ae5fede513b3cf972bd72140fcd7910
NikMozd <mozdorov@yandex.ru>
2020-02-26 01:54:47 +0300

    Task 2, part 1, part 2 (a bit)

--- Task2/Form1.Designer.cs
+++ Task2/Form1.Designer.cs
@@ -28,13 +28,279 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(413, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(360, 270);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(120, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Исходное изображение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(487, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Полученное изображение";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(837, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Преобразование к полутоновому 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 35);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Преобразование к полутоновому 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(838, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Бинаризация верхняя";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(838, 199);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(163, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Бинаризация нижняя";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(838, 228);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(163, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Бинаризация по диапазону";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(849, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(852, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Порог 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(935, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Порог 2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(932, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(837, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(163, 48);
+            this.panel1.TabIndex = 15;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(153, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Полутоновое усреднение";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(156, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Полутоновое с разн коэф";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(838, 274);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(162, 35);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Бинаризация Оцу глобальная";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(838, 315);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(162, 35);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Бинаризация Оцу\r\nлокальная";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(838, 356);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(162, 35);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "Бинаризация Оцу иерархическая";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1049, 483);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- Task2/Form1.cs
+++ Task2/Form1.cs
@@ -12,9 +12,179 @@ namespace Task2
 {
     public partial class Form1 : Form
     {
+        private System.Drawing.Imaging.BitmapData bmpData;
+        private int bytesCount;
+
+        private const int segmentCountWidth = 3;
+        private const int segmentCountHeight = 2;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private byte[] GetImageBytes()
+        {
+            if (pictureBox1.Image == null)
+                throw new Exception("Пустое исходное изображение");
+
+            pictureBox2.Image = pictureBox1.Image.Clone() as Image;
+            var bmp = (Bitmap)pictureBox2.Image;
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp.PixelFormat);
+            bytesCount = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] res = new byte[bytesCount];
+            System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, res, 0, bytesCount);
+            return res;
+        }
+
+        private byte[] ColorBytesToGrayShades(byte[] bytes)
+        {
+            if (radioButton1.Checked)
+                return bytes
+                .Select((b, i) => new { B = b, I = i })
+                .GroupBy(tmp => tmp.I / 3, tmp => tmp.B)
+                .Select(bt => (byte)(bt.Select(b => (int)b).Sum() / 3))
+                .SelectMany(b => new[] { b, b, b })
+                .ToArray();
+            if (radioButton2.Checked)
+                return bytes
+                .Select((b, i) => new { B = b, I = i })
+                .GroupBy(tmp => tmp.I / 3, tmp => tmp.B)
+                .Select(bt => bt.ToArray())
+                .Select(bt => (byte)(0.3 * bt[2] + 0.59 * bt[1] + 0.11 * bt[0]))
+                .SelectMany(b => new[] { b, b, b })
+                .ToArray();
+            throw new ArgumentException("Нужно выбрать метод перевода в оттенки серого");
+        }
+
+        private byte[] GetImageBytesGray() => ColorBytesToGrayShades(GetImageBytes());
+
+        private void SetImageBytes(byte[] bytes)
+        {
+            var bmp = (Bitmap)pictureBox2.Image;
+            System.Runtime.InteropServices.Marshal.Copy(bytes, 0, bmpData.Scan0, bytesCount);
+            bmp.UnlockBits(bmpData);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var diag = new OpenFileDialog())
+            {
+                if (diag.ShowDialog() == DialogResult.OK)
+                    pictureBox1.Image = new Bitmap(diag.FileName);
+            }
+
+            var bytes = GetImageBytes();
+            var bytesNew = ColorBytesToGrayShades(bytes);
+            SetImageBytes(bytesNew);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var bytes = GetImageBytes();
+            var bytesNew = bytes
+                .Select((b, i) => new { B = b, I = i })
+                .GroupBy(tmp => tmp.I / 3, tmp => tmp.B)
+                .Select(bt => (byte)(bt.Select(b => (int)b).Sum() / 3))
+                .SelectMany(b => new[] { b, b, b })
+                .ToArray();
+            SetImageBytes(bytesNew);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var bytes = GetImageBytes();
+            var bytesNew = bytes
+                .Select((b, i) => new { B = b, I = i })
+                .GroupBy(tmp => tmp.I / 3, tmp => tmp.B)
+                .Select(bt => bt.ToArray())
+                .Select(bt => (byte)(0.3 * bt[2] + 0.59 * bt[1] + 0.11 * bt[0]))
+                .SelectMany(b => new[] { b, b, b })
+                .ToArray();
+            SetImageBytes(bytesNew);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!byte.TryParse(textBox1.Text, out var t1))
+                throw new ArgumentException("Некорректный ввод порога 1");
+            var bytes = GetImageBytesGray();
+            var bytesNew = bytes.Select(b => (byte)(b >= t1 ? 255 : 0)).ToArray();
+            SetImageBytes(bytesNew);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!byte.TryParse(textBox1.Text, out var t1))
+                throw new ArgumentException("Некорректный ввод порога 1");
+            var bytes = GetImageBytesGray();
+            var bytesNew = bytes.Select(b => (byte)(b >= t1 ? 0 : 255)).ToArray();
+            SetImageBytes(bytesNew);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!byte.TryParse(textBox1.Text, out var t1))
+                throw new ArgumentException("Некорректный ввод порога 1");
+            if (!byte.TryParse(textBox2.Text, out var t2))
+                throw new ArgumentException("Некорректный ввод порога 2");
+            if (t1 > t2)
+                throw new ArgumentException("Порог 1 не должен быть больше порога 2");
+            var bytes = GetImageBytesGray();
+            var bytesNew = bytes.Select(b => (byte)(t1 <= b && b <= t2 ? 255 : 0)).ToArray();
+            SetImageBytes(bytesNew);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var bytes = GetImageBytesGray();
+            var hist = new double[256];
+            foreach (var b in bytes)
+                hist[b]++;
+            
+            hist = hist.Select(h => h / bytes.Length).ToArray();
+            
+            var psum = new double[256];
+            var msum = new double[256];
+            psum[0] = hist[0];
+            for (int i = 1; i < 256; ++i)
+            {
+                psum[i] = psum[i - 1] + hist[i];
+                msum[i] = msum[i - 1] + i * hist[i];
+            }
+
+            var mg = msum.Last();
+            var sigma = new double[256];
+            for (int i = 0; i < 256; ++i)
+            {
+                sigma[i] = Math.Pow(mg * psum[i] - msum[i], 2) / (psum[i] * (1 - psum[i]));
+                if (double.IsNaN(sigma[i]))
+                    sigma[i] = 0;
+            }
+
+            var threshold = sigma.ToList().IndexOf(sigma.Max());
+            var bytesNew = bytes.Select(b => (byte)(b >= threshold ? 255 : 0)).ToArray();
+            SetImageBytes(bytesNew);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var bytes = GetImageBytesGray();
+            List<byte>[] segments = new List<byte>[6];
+            for (int i = 0; i < 6; ++i)
+                segments[i] = new List<byte>();
+
+            var width = bmpData.Stride / segmentCountWidth;
+            var height = pictureBox2.Image.Height / segmentCountHeight;
+            var tmp = bytes.Select((b, i) => new 
+            { 
+                Row = (i / bmpData.Stride) / height, 
+                Column = (i % bmpData.Stride) / width, 
+                Value = b
+            });
+
+            // ?????
         }
     }
 }
